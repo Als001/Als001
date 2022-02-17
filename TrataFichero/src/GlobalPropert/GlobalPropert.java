330e71ccb408b070639dbc1acc830d7657bed0d5
AlbertoS <a.sedano.lucio@ibermatica.com>
2022-02-17 12:36:23 +0100

    Comentarios

--- TrataFichero/src/GlobalPropert/GlobalPropert.java
+++ TrataFichero/src/GlobalPropert/GlobalPropert.java
@@ -12,6 +12,8 @@ package GlobalPropert;
 
 public class GlobalPropert {
 
+// aquí se indica la dirección de los ficheros a usar en la clase Java TratarFichero
+
 public enum Proper {
    FICHERO1("C:/Java/Prueba.txt"),
    FICHERO2("C:/Java/Prueba2.txt"),
